c8e4344991a3f31e29e5943784bb99f4c73be121
Deena Swaminathan K <44472854+DeenaSwaminathanKumaravel@users.noreply.github.com>
2024-05-03 22:30:50 +0530

    Add files via upload

--- SQL/Day 02 - String & Date Functions.sql
+++ SQL/Day 02 - String & Date Functions.sql
@@ -16,7 +16,7 @@ ALTER TABLE Students
 DROP TABLE IF EXISTS Students ;
 
 -- Denormalization examples
-SELECT * FROM data_analytics_duo.employee;
+SELECT * FROM employee;
 
 SELECT * FROM employee a
 LEFT JOIN department b ON a.DepartmentID = b.DepartmentID;
@@ -34,15 +34,20 @@ DROP DepartmentName;
 
 SET SQL_SAFE_UPDATES = 0;
 
+select * from employee;
+
 UPDATE employee a
 LEFT JOIN department b ON a.DepartmentID = b.DepartmentID
-SET a.DepartmentName = b.DepartmentName
+SET b.DepartmentName = b.DepartmentName
 WHERE a.DepartmentID IS NOT NULL;
 
 SET SQL_SAFE_UPDATES = 1;
 
 ALTER TABLE employee
 DROP DepartmentName; 
+
+insert into department 
+values(106, "Analytics");
 
 -- Rollback
 UPDATE employee
@@ -59,6 +64,77 @@ ALTER TABLE employee
 DROP ManagerID;
 
 ROLLBACK;
+
+-- String Functions
+
+-- concatenate
+select concat(FirstName, " ", UPPER(LastName)) FullName, FirstName, LastName from employee;
+
+-- using substring for Proper()
+
+select 
+concat(
+concat(lcase( substring(FirstName, 1, 1)), ucase( substring(FirstName, 2, char_length(FirstName)))), " ", -- Firstname
+
+concat(lcase( substring(LastName, 1, 1)), ucase( substring(LastName, 2, char_length(LastName)))) -- Lastname
+) FullName, FirstName, LastName
+
+from employee; 
+
+select Email, length(Email) from employee;
+-- using position on subtring to inmitate delimiter as "@"
+
+select Email, 
+	substring(Email, 1, position("@" IN Email)-1 ) as emailname, 
+	substring(Email, position("@" in Email)+ 1, char_length(Email)) as DomainName
+from employee;
+
+select Email, substring(Email, 1, instr(Email, "@") ) from employee; 	
+
+select Salary, format(Salary, 1) as format_fn from employee; 
+
+select * from employee;
+
+
+select DateofBirth, abs((datediff(DateofBirth, CURDATE())/365)) from employee e; 
+
+-- Floor
+select DateofBirth, floor(abs((datediff(DateofBirth, CURDATE())/365))) from employee e; 
+
+
+-- Date Functions --
+
+select * from employee;
+
+select date_format(DateOfBirth, "%b-%y") from employee e order by DateOfBirth desc; 
+
+
+select * from employee e ;
+
+select DateOfBirth, Date(concat( year(DateOfBirth),  "-", month(DateOfBirth), "-", 1)) as Firstday,
+	date_add(Date(concat( year(DateOfBirth),  "-", month(DateOfBirth), "-", 1)), interval 1 month) as NextMonth 
+from employee e ; 
+
+--  using dateadd and datesub
+
+select date_add(DateOfBirth, interval 10 day) as d_add, 
+	date_sub(DateOfBirth, interval 10 day) as d_sub,
+	date_add(DateOfBirth, interval -10 day) as d_sub_using_add, 
+	date_sub(DateOfBirth, interval +10 day) as d_sub_using_sub,
+	DateOfBirth from employee; 
+
+
+select last_day(DateOfBirth) as lastday, 
+Date(concat(year(DateOfBirth), "-", month(DateOfBirth),"-", "1")) as firstday, 
+DateOfBirth from employee;
+
+-- (OR)
+
+select last_day(DateOfBirth) as lastday, 
+date(date_format(DateOfBirth, "%Y-%m-01")) firstday, 
+DateOfBirth from employee;
+
+select dayname(DateOfBirth), DateOfBirth from employee; 
 
 
 
