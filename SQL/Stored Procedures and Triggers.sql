8417af4f2edb944cbb6729b43bcc35198a8b8192
Deena Swaminathan K <44472854+DeenaSwaminathanKumaravel@users.noreply.github.com>
2024-05-22 23:39:23 +0530

    Add files via upload

--- SQL/Stored Procedures and Triggers.sql
+++ SQL/Stored Procedures and Triggers.sql
@@ -89,3 +89,40 @@ Delimiter ;
 
 insert into employee (EmployeeID, FirstName, LastName, Email)
 values(100, 'guy', 'Might', 'thisisatestforinserttrigger@gmail.com');
+
+
+-- views and Materialized View
+
+create view maxsalary_emp as
+select EmployeeID, FirstName, LastName, Email, Salary from employee e 
+where Salary > 60000
+order by Salary desc;
+
+
+select * from maxsalary_emp me ;
+
+-- Materialized view
+
+/* This syntax wont work as MS SQL Server
+ * 
+ * 
+ * CREATE MATERIALIZED VIEW m_maxsalary_emp ASs
+SELECT EmployeeID, FirstName, LastName, Email, Salary
+FROM employee
+WHERE Salary > 62000;
+
+ */
+
+set @variableexp = 'Hello Flickers! (its a bird)'
+
+select @variableexp
+
+
+
+	 # Write your MySQL query statement below
+select
+	round((select count(distinct player_id) from activity
+where (player_id, event_date) in
+(select player_id, date_add(min(event_date), interval 1 day) from activity group by player_id)
+/
+select count(distinct player_id) from activity), 2) fraction
